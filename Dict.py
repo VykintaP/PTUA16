a88034ee695020962e88cb7e2bc963aa422eb254
VykintaP <vykinta.parciauskaite@gmail.com>
2024-11-28 21:45:18 +0200

    first commit

---
+++ Dict.py
@@ -0,0 +1,16 @@
+# viena_puse_keys = ["miestas", "amžius", "lytis"]
+# kita_puse_values = ["Keiptaunas", "98", "Jūs"]
+# uztrauktukas = dict(zip(viena_puse_keys, kita_puse_values))
+# print(uztrauktukas)
+#
+# my_dict = {"name": "Alicija", "age": 25, "city": "Niujorkas"}
+# value_to_find = "Alicija"
+# key = next((k for k, v in my_dict.items() if v == value_to_find), None)
+# print(key)  # Rezultatas: name
+#
+#
+# stalciuko_turinys = "Keiptaunas"
+# stalciuko_turinys = "98"
+# koks_raktas = next((k for k, v in uztrauktukas.items() if v == stalciuko_turinys))
+# print(koks_raktas)
+# print(uztrauktukas.items())
